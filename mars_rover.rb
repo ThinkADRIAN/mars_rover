16c7876506673a29c9b475fbc53bfd4cdc4a0a18
Adrian Cunanan <adrian@adriancunanan.com>
2014-10-08 13:08:12 -0400

    Adjust Method Naming Convention

--- mars_rover.rb
+++ mars_rover.rb
@@ -9,56 +9,56 @@ class MarsRover
 	end
 
 	# instance methods
-	def readInstructionLine(file,split_char)
+	def read_instruction_line(file,split_char)
 		line = file.readline
 		line = line.chomp!
 		line = line.split(split_char)
 	end
 
-	def setUpRightCoordinates(line_reader)
+	def set_up_right_coordinates(line_reader)
 		@x_up_right = line_reader[0]
 		@y_up_right = line_reader[1]
 	end
 
 	# read first line for Top Right Coordinates
-	def handleTopRightCoordsInstruction(file)
-		line_reader = self.readInstructionLine(file, " ")
-		self.setUpRightCoordinates(line_reader)
+	def handle_top_right_coords_instruction(file)
+		line_reader = self.read_instruction_line(file, " ")
+		self.set_up_right_coordinates(line_reader)
 	end
 
-	def setRoverStartCoordinates(line_reader)
+	def set_rover_start_coordinates(line_reader)
 		@x_coord = line_reader[0].to_i
 		@y_coord = line_reader[1].to_i
 		@heading = line_reader[2]
 	end
 
 	# read line for Rover Position
-	def handleRoverPositionInstruction(file)
-		line_reader = self.readInstructionLine(file, " ")
-		self.setRoverStartCoordinates(line_reader)
+	def handle_rover_position_instruction(file)
+		line_reader = self.read_instruction_line(file, " ")
+		self.set_rover_start_coordinates(line_reader)
 	end
 
 	# If spin_direction is left then left_spin_heading value will be evaluated
 	# or if spin_direction is right then right_spin_heading will evaluated
 	# i.e. Left Turn from N or Right Turn from S results in W
-	def evaluateSpin(spin_direction,left_spin_heading, right_spin_heading)
+	def evaluate_spin(spin_direction,left_spin_heading, right_spin_heading)
 		@heading == left_spin_heading && spin_direction == "left" || 
 			@heading == right_spin_heading && spin_direction == "right"
 	end
 
 	def spin(direction)
-		if self.evaluateSpin(direction,"N","S")
+		if self.evaluate_spin(direction,"N","S")
 			@heading = "W"
-		elsif self.evaluateSpin(direction,"W","E")
+		elsif self.evaluate_spin(direction,"W","E")
 			@heading = "S"
-		elsif self.evaluateSpin(direction,"S","N")
+		elsif self.evaluate_spin(direction,"S","N")
 			@heading = "E"
-		elsif self.evaluateSpin(direction,"E","W")
+		elsif self.evaluate_spin(direction,"E","W")
 			@heading = "N"
 		end	
 	end
 
-	def moveForward
+	def move_forward
 		if @heading == "E"
 			@x_coord += 1
 		elsif @heading == "N"
@@ -70,40 +70,40 @@ class MarsRover
 		end	
 	end
 
-	def parseInstructions(instruction_set)
-		instruction_set.each { |i| 
+	def parse_instructions(instruction_set)
+		instruction_set.each do |i| 
 			if i == "L"
 				self.spin("left")
 			elsif i == "R"
 				self.spin("right")
 			elsif i == "M"
-				self.moveForward
+				self.move_forward
 			end		
-		}
+		end
 	end
 
-	def outputRoverCoordinates
+	def output_rover_coordinates
 		puts @x_coord.to_s + " " + @y_coord.to_s + " " + @heading
 	end
 	
 	# read line for Rover Instructions and Update Rover Position
-	def handleRoverMoveInstructions(file)
-		line_reader = self.readInstructionLine(file, "")
-		self.parseInstructions(line_reader)
-		self.outputRoverCoordinates
+	def handle_rover_move_instructions(file)
+		line_reader = self.read_instruction_line(file, "")
+		self.parse_instructions(line_reader)
+		self.output_rover_coordinates
 	end
 
-	def handleInstructionFile(file)
-		self.handleTopRightCoordsInstruction(file)
+	def handle_instruction_file(file)
+		self.handle_top_right_coords_instruction(file)
 
 		while !file.eof?
-		  self.handleRoverPositionInstruction(file)
-			self.handleRoverMoveInstructions(file)
+		  self.handle_rover_position_instruction(file)
+			self.handle_rover_move_instructions(file)
 		end
 	end
 end
 
 my_rover = MarsRover.new
 file = File.open("mars_rover_input.txt", 'r')
-my_rover.handleInstructionFile(file)
+my_rover.handle_instruction_file(file)
 file.close
